2be764bbb64f530d61b9060902a34b336858cb0b
DiasFlorian <florian.dias@viacesi.fr>
2021-10-11 14:25:27 +0200

    New button

---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="Test button"/>
+    </div>
+  );
+}
+
+export default App;
