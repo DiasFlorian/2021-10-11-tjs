4efdaea84d0b733f1fb69ce5f612b0142a47de15
DiasFlorian <florian.dias@viacesi.fr>
2021-10-11 14:46:24 +0200

    Suppresion bouton 2

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
-import Button2 from './components/Button2/Button';
   
 
 function App() {
   return (
     <div className="App">
       <Button text="Test button"/>
-      <Button2 text="Test button 2"/>
     </div>
   );
 }
