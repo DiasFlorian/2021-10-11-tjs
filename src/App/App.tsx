b36e056b83bde76e0a6ad64806a6529b5001d4b9
DiasFlorian <florian.dias@viacesi.fr>
2021-10-11 15:38:01 +0200

    Utilisation de PropTypes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,11 +6,13 @@ import Button from './components/Button/Button';
 function App() {
   return (
     <div className="App">
-      <Button>
+      <Button onclickevent={ function (argument1) {
+        console.log(argument1);
+      } }>
         <img alt="" src="https://cdn.pixabay.com/photo/2019/09/23/04/50/jellyfish-4497496_960_720.jpg"/> 
         Cliquez ici
       </Button>
-      <Button text="Un bouton"/>
+      <Button onclickevent={()=>{}} text="Un bouton"/>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
 import styles from './Button.module.css';
+import PropTypes from 'prop-types';
 
 const Button = (props) => {
     console.log(props);
     return (
-        <button type={props.type} className={styles.Button}>
+        <button type={props.type} className={styles.Button} onClick={(evt)=>{
+            props.onclickevent('C good on a cliqué sur moi, je te préviens père');
+        }}>
             {undefined!==props.children?props.children:props.text}        
         </button>
     );
 }
+Button.propTypes = {
+    onclickevent: PropTypes.func.isRequired,
+    text: PropTypes.string,
+    children: PropTypes.any
+};
 
 export default Button;
