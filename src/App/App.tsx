f56f4ec63cf4c7433f22a5d6e1391770721a987b
DiasFlorian <florian.dias@viacesi.fr>
2021-10-12 16:48:15 +0200

    utilisation de fetch

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/ui/Navbar/Navbar';
 import MemeViewer, { demoMeme as defaultmeme } from './components/ui/MemeViewer/MemeViewer';
 import MemeForm from './components/functionnal/MemeForm/MemeForm';
 import FlexLayout from './components/layouts/FlexLayout/FlexLayout';
+import { ADR_SRV, RESSOURCES_NAME } from './config/config';
 
 
 interface Props {
@@ -18,7 +19,13 @@ interface State {
 export default class App extends Component<Props, State> {
   state = { memes: [], images: [] }
   componentDidMount() {
-    fetch('').then()
+    const pmemes = fetch(`${ADR_SRV}${RESSOURCES_NAME.memes}`).then(f => f.json());
+    const pmimages = fetch(`${ADR_SRV}${RESSOURCES_NAME.images}`).then(f => f.json());
+    Promise.all([pmemes,pmimages])
+    .then(ar_ar=>{
+      this.setState({memes:ar_ar[0], images:ar_ar[1]});
+      return ar_ar;
+    })
   }
 
   componentDidUpdate() {
