888b9458ca2eeb46a3382759efc1356868577e71
DiasFlorian <florian.dias@viacesi.fr>
2021-10-12 16:36:14 +0200

    config avec enum freeze

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Button from './components/ui/Button/Button';
 import Navbar from './components/ui/Navbar/Navbar';
-import MemeViewer, {demoMeme as defaultmeme} from './components/ui/MemeViewer/MemeViewer';
+import MemeViewer, { demoMeme as defaultmeme } from './components/ui/MemeViewer/MemeViewer';
 import MemeForm from './components/functionnal/MemeForm/MemeForm';
 import FlexLayout from './components/layouts/FlexLayout/FlexLayout';
 
@@ -11,30 +11,32 @@ interface Props {
 
 }
 interface State {
-  counter: number
+  memes: Array<{}>,
+  images: Array<{}>
 }
 
 export default class App extends Component<Props, State> {
-  state = { counter: 0 }
-  componentDidMount(){
+  state = { memes: [], images: [] }
+  componentDidMount() {
+    fetch('').then()
   }
-  
-  componentDidUpdate(){
+
+  componentDidUpdate() {
     console.log(arguments);
     console.log(this.state);
   }
   render() {
     return (
       <>
-      <div>{JSON.stringify(this.state)}</div>
+        <div>{JSON.stringify(this.state)}</div>
 
-      <div className="App">
-        <Navbar></Navbar>
-        <FlexLayout>
-          <MemeViewer meme={defaultmeme.meme} image={defaultmeme.image}/>
-          <MemeForm />
-        </FlexLayout>
-      </div>
+        <div className="App">
+          <Navbar></Navbar>
+          <FlexLayout>
+            <MemeViewer meme={defaultmeme.meme} image={defaultmeme.image} />
+            <MemeForm />
+          </FlexLayout>
+        </div>
       </>
     )
   }
