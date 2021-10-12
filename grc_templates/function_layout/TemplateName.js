399032cc2dd3f459d0b3fd835c2469f50047c43d
DiasFlorian <florian.dias@viacesi.fr>
2021-10-12 12:10:28 +0200

    Utilisation des templates poru faire des patrons

---
+++ grc_templates/function_layout/TemplateName.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TEmplateName.module.css'
+
+
+function TemplateName(props) {
+    return (
+        <div className={styles.Templatename} data-testid="TemplateName" style={{...props.style}} >
+            {props.children}
+        </div>
+    );
+
+}
+
+
+TemplateName.propTypes = {
+children: PropTypes.any.isRequired,
+style: PropTypes.object
+};
+
+
+export default TemplateName;
